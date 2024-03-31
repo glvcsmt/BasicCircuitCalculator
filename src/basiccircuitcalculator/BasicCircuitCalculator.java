c8147fc45ed8c75c8b1086f1bfacdf18d625fdeb
glvcsmt <>
2024-03-31 17:51:08 +0200

    Finished 0.1 version of the circuit calculator. Works fine, checked the outputs on paper.

---
+++ src/basiccircuitcalculator/BasicCircuitCalculator.java
@@ -0,0 +1,13 @@
+package basiccircuitcalculator;
+
+/**
+ *
+ * @author glvcsmt
+ */
+public class BasicCircuitCalculator {
+    
+    public static void main(String[] args) {
+        new Window().setVisible(true);
+    }
+    
+}
